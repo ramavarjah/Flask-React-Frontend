2b07a1468295d53a74c4c6aeb955891fa5f48a33
Satya Venkat <cshekar.madhu@gmail.com>
2020-11-25 07:39:33 +0530

    first commit

---
+++ src/components/Card/card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export const Card = ({listOfTodos})=> {
+    return(
+    <>
+    {listOfTodos.map(todo => {
+        return(
+            <ul key={todo.id}>
+                <li>{todo.content}</li>
+
+            </ul>
+        )
+    })}
+    </>
+    ) 
+}
---
+++ src/components/Pages/TodoPage.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import {Card} from  '../Card/card';
+import {Form} from  '../Form/form';
+
+
+export const TodoPage = ()=> {
+
+    const [todo, setTodo] = useState([])
+    const [addTodo, setAddTodo] = useState('')
+
+    useEffect(()=>{
+        fetch('/api').then(response=> {
+            if (response.ok){
+                return response.json()
+            }
+        }).then(data => setTodo(data))
+
+    },[])
+
+    const handleFormChange = (inputValue) => {
+        setAddTodo(inputValue)
+        console.log(addTodo)
+    }
+
+    const handleFormSubmit = () => {
+        fetch('/api/create',{
+            method: 'POST',
+            body: JSON.stringify({
+                content:addTodo
+
+            }),
+            headers:{
+                 "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(response => response.json())
+           .then(message =>{
+            console.log(message)
+            setAddTodo('')
+            getLatestTodos()
+           } )
+    }
+
+    const getLatestTodos = () => {
+        fetch('/api').then(response => {
+            if(response.ok){
+                return response.json() 
+            }
+        }).then(data => setTodo(data))
+    }
+
+    return(
+        <>
+        <Form userInput={addTodo } onFormChange ={handleFormChange} onFormSubmit ={handleFormSubmit}/>
+        <Card listOfTodos={todo}/>
+        </>
+    )
+}
